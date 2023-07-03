85f9e3b0e10b3ff30323199ad62e9cb614eee2ca
Vlad Kupchik <vladkupchik15@gmail.com>
2023-07-03 04:03:14 +0300

    2

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,11 +2,13 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        if(first > second){
-            System.out.print(first);
+        if(first > second || second > first){
+            if(first > second){
+            System.out.print(first);}else
+            {
+                System.out.print(second);
+            }
         } else if (first == second) {
-            System.out.print(second);
-        }else{
             System.out.print(second);
         }
     }
